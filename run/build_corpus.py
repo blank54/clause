ed1ba1f595530b5f38d7fa71a5cc76500f0b8b2b
SeonghyeonMoon <blank54@snu.ac.kr>
2021-09-03 09:57:24 +0900

    build clause corpus

---
+++ run/build_corpus.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Configuration
+import os
+import sys
+rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1])
+sys.path.append(rootpath)
+
+from object import Doc
+from clutil import ClPath
+clpath = ClPath()
+
+import pandas as pd
+import pickle as pk
+from collections import defaultdict
+
+
+def build_corpus(fpath_data):
+    corpus = []
+    df = pd.read_excel(fpath_data)
+    for idx, row in df.iterrows():
+        clause = Doc(tag=row['tag'], text=row['text'])
+        corpus.append(clause)
+    return corpus
+
+def save_corpus(corpus):
+    fname_corpus = 'clause.pk'
+    fpath_corpus = os.path.join(clpath.fdir_corpus, fname_corpus)
+    with open(fpath_corpus, 'wb') as f:
+        pk.dump(corpus, f)
+
+    print('Build corpus')
+    print('  | fdir : {}'.format(clpath.fdir_corpus))
+    print('  | fname: {}'.format(fname_corpus))
+
+
+if __name__ == '__main__':
+    fname_data = 'clause.xlsx'
+    fpath_data = os.path.join(clpath.fdir_data, fname_data)
+
+    corpus = build_corpus(fpath_data=fpath_data)
+    save_corpus(corpus=corpus)
