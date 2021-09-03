eec9cba47664fcfb7bd5a084ef359114b503ae20
SeonghyeonMoon <blank54@snu.ac.kr>
2021-09-03 15:34:49 +0900

    normalization

--- run/build_corpus.py
+++ run/build_corpus.py
@@ -8,8 +8,9 @@ rootpath = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.
 sys.path.append(rootpath)
 
 from object import Doc
-from clutil import ClPath
+from clutil import ClPath, ClFunc
 clpath = ClPath()
+clfunc = ClFunc()
 
 import pandas as pd
 import pickle as pk
@@ -21,9 +22,13 @@ def build_corpus(fname_data):
 
     corpus = []
     df = pd.read_excel(fpath_data)
+
+    print('Build corpus')
     for idx, row in df.iterrows():
-        clause = Doc(tag=row['tag'], text=row['text'])
+        clause = Doc(tag=row['tag'], text=row['text'], normalized_text=clfunc.normalize(text=row['text']))
         corpus.append(clause)
+        print('\r  | Normalization: {:,d}'.format(idx+1), end='')
+    print('\n  | Total {:,d} clauses'.format(len(corpus)))
     return corpus
 
 def save_corpus(corpus, fname_corpus):
@@ -31,7 +36,7 @@ def save_corpus(corpus, fname_corpus):
     with open(fpath_corpus, 'wb') as f:
         pk.dump(corpus, f)
 
-    print('Build corpus')
+    print('Save corpus')
     print('  | fdir : {}'.format(clpath.fdir_corpus))
     print('  | fname: {}'.format(fname_corpus))
 
