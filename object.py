8b37ffdb08359ec548eea9a40f8d735aa12e62f0
SeonghyeonMoon <blank54@snu.ac.kr>
2021-09-03 09:57:12 +0900

    update metadata of Doc

--- object.py
+++ object.py
@@ -9,6 +9,16 @@ class Doc:
         self.tag = tag
         self.text = text
 
+        self.country = tag.split('_')[0]
+        self.state = tag.split('_')[1]
+        self.year = tag.split('_')[2]
+        self.chapter_id = tag.split('_')[3]
+        self.section_id = tag.split('_')[4]
+        self.clause_id = '_'.join(tag.split('_')[5:])
+
+        self.region = '{}_{}'.format(self.country, self.state)
+        self.section_tag = '{}_{}_{}_{}_{}'.format(self.country, self.state, self.year, self.chapter_id, self.section_id)
+
     def __str__(self):
         return self.text
 
