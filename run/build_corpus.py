bda622c89323f81883bae2dcd46c87d7a7eebf81
Boris <blank54@snu.ac.kr>
2021-09-29 16:55:36 +0900

    add labeled data (#=120)

--- run/build_corpus.py
+++ run/build_corpus.py
@@ -12,6 +12,7 @@ from provutil import ProvPath, ProvFunc
 provpath = ProvPath()
 provfunc = ProvFunc()
 
+import json
 import pandas as pd
 import pickle as pk
 from collections import defaultdict
@@ -31,10 +32,69 @@ def build_corpus(fname_data):
     print('\n  | Total {:,d} provisions'.format(len(corpus)))
     return corpus
 
+def export_data_for_labeling(corpus):
+    data = []
+    for doc in corpus:
+        data.append('  TAGSPLIT  '.join((doc.tag, doc.text)))
+
+    fname_data_for_labeling = 'provision_for_labeling.txt'
+    fpath_data_for_labeling = os.path.join(provpath.fdir_data, fname_data_for_labeling)
+    with open(fpath_data_for_labeling, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(data))
+
+    print('============================================================')
+    print('Convert corpus to data_for_labeling')
+    print('  | fdir : {}'.format(provpath.fdir_data))
+    print('  | fname: {}'.format(fname_data_for_labeling))
+
+def read_labeled_data(fname_provision_labeled):
+    fpath_provision_labeled = os.path.join(provpath.fdir_data, fname_provision_labeled)
+    with open(fpath_provision_labeled, 'r') as f:
+        labeled_data = [json.loads(line) for line in list(f)]
+    return labeled_data
+
+def assign_labels(corpus, labeled_data):
+    print('============================================================')
+    print('Assign labels to corpus')
+    print('  | # of corpus            : {:,}'.format(len(corpus)))
+    print('  | # of labeled data      : {:,}'.format(len(labeled_data)))
+
+    corpus_labeled = []
+    for line in labeled_data:
+        tag = line['text'].split('TAGSPLIT')[0].strip()
+        doc = [doc for doc in corpus if doc.tag == tag][0]
+        doc.labels = line['accept']
+        corpus_labeled.append(doc)
+
+    print('  | # of corpus with labels: {:,}'.format(len(corpus_labeled)))
+    return corpus_labeled
+
+def verify_labels(corpus_labeled):
+    print('============================================================')
+    print('Verify labels')
+
+    for doc in corpus_labeled[:5]:
+        print('--------------------------------------------------')
+        print('  | Tag   : {}'.format(doc.tag))
+        print('  | Text  : {}...'.format(doc.text[:50]))
+        print('  | Labels: {}'.format(', '.join(doc.labels)))
+
+
 
 if __name__ == '__main__':
     fname_data = 'provision.xlsx'
     fname_corpus = 'provision.pk'
+    fname_corpus_labeled = 'provision_labeled.pk'
+    fname_provision_labeled = 'provision_labeled.jsonl'
 
+    ## Initialize corpus
     corpus = build_corpus(fname_data=fname_data)
-    provfunc.save_corpus(corpus=corpus, fname_corpus=fname_corpus)
+
+    ## Export data for labeling
+    export_data_for_labeling(corpus=corpus)
+
+    ## Assign labels
+    labeled_data = read_labeled_data(fname_provision_labeled=fname_provision_labeled)
+    corpus_labeled = assign_labels(corpus=corpus, labeled_data=labeled_data)
+    provfunc.save_corpus(corpus=corpus_labeled, fname_corpus=fname_corpus_labeled)
+    verify_labels(corpus_labeled)
