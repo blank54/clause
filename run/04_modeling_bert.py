98e31e4028ae2e4aec0973a0d977ffe0bb10c8f5
Boris <blank54@snu.ac.kr>
2022-01-03 09:57:27 +0900

    modeling bert2 hotfix:fix: save test results

--- run/04_modeling_bert.py
+++ run/04_modeling_bert.py
@@ -3,6 +3,7 @@
 
 '''
 http://yonghee.io/bert_binary_classification_naver/
+https://nomalcy.tistory.com/211
 '''
 
 # Configuration
@@ -17,7 +18,7 @@ clausefunc = ClauseFunc()
 clauseeval = ClauseEval()
 
 import torch
-from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler
 
 from transformers import BertForSequenceClassification, AdamW
 from transformers import get_linear_schedule_with_warmup
@@ -26,9 +27,10 @@ import random
 import numpy as np
 import pickle as pk
 from collections import defaultdict
+from sklearn.metrics import accuracy_score
 
 
-def build_dataloader(inputs, labels, masks, target_label, option, encode):
+def build_dataloader(inputs, labels, masks, target_label, encode):
     global BATCH_SIZE
 
     inputs = torch.tensor(inputs)    
@@ -40,19 +42,16 @@ def build_dataloader(inputs, labels, masks, target_label, option, encode):
         labels = torch.tensor(labels)
 
     data = TensorDataset(inputs, masks, labels)
-
-    if option == 'train':
-        sampler = RandomSampler(data)
-    elif option == 'valid':
-        sampler = SequentialSampler(data)
-    elif option == 'test':
-        sampler = RandomSampler(data)
+    sampler = RandomSampler(data)
 
     return DataLoader(data, sampler=sampler, batch_size=BATCH_SIZE)
 
 def gpu_allocation():
     print('============================================================')
     print('GPU allocation')
+
+    os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES']= '2'
 
     if torch.cuda.is_available():    
         device = torch.device('cuda')
@@ -64,7 +63,7 @@ def gpu_allocation():
 
     return device
 
-def model_training(train_dataloader, valid_dataloader, test_dataloader):
+def model_training(train_dataloader, valid_dataloader):
     global RANDOM_STATE, EPOCHS, DEVICE, target_label
 
     print('------------------------------------------------------------')
@@ -72,53 +71,50 @@ def model_training(train_dataloader, valid_dataloader, test_dataloader):
 
     model = BertForSequenceClassification.from_pretrained('bert-base-multilingual-cased', num_labels=2)
     model.cuda()
+    result = defaultdict(list)
 
-    optimizer = AdamW(model.parameters(),
-                      lr=LEARNING_RATE, #학습률
-                      eps=1e-8 #0으로 나누는 것을 방지하기 위한 epsilon 값
-                      )
+    optimizer = AdamW(model.parameters(), lr=LEARNING_RATE, eps=1e-8)
+    
 
     epochs = EPOCHS
-    total_steps = len(train_dataloader) * epochs #총 훈련 스텝
-    scheduler = get_linear_schedule_with_warmup(optimizer, 
-                                                num_warmup_steps=0,
-                                                num_training_steps=total_steps)
-
+    total_steps = len(train_dataloader) * epochs
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+    
     random.seed(RANDOM_STATE)
     np.random.seed(RANDOM_STATE)
     torch.manual_seed(RANDOM_STATE)
     torch.cuda.manual_seed_all(RANDOM_STATE)
 
-    result = defaultdict(list)
     model.zero_grad()
     for epoch in range(EPOCHS):
         train_loss = 0
+        train_accuracy = 0
         valid_loss = 0
-
-        valid_accuracy_epoch_total = 0
-        nb_valid_steps = 0
-
-        test_accuracy_epoch_total = 0
-        nb_test_steps = 0
+        valid_accuracy = 0
 
         model.train()   
         for step, batch in enumerate(train_dataloader):
             batch = tuple(t.to(DEVICE) for t in batch)
             b_input_ids, b_input_mask, b_labels = batch
 
-            outputs = model(b_input_ids, 
-                            token_type_ids=None, 
-                            attention_mask=b_input_mask, 
-                            labels=b_labels)
+            # forward
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
 
-            loss = outputs[0] # 로스 구함
-            train_loss += loss.item() # 총 로스 계산
+            # loss
+            train_loss_batch = outputs.loss
+            train_loss += train_loss_batch.item()
 
-            loss.backward() # Backward 수행으로 그래디언트 계산
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # 그래디언트 클리핑
-            optimizer.step() # 그래디언트를 통해 가중치 파라미터 업데이트
-            scheduler.step() # 스케줄러로 학습률 감소
-            model.zero_grad() # 그래디언트 초기화
+            # accuracy
+            pred = [torch.argmax(logit).cpu().detach().item() for logit in outputs.logits]
+            true = [label for label in b_labels.cpu().numpy()]
+            train_accuracy += accuracy_score(true, pred)
+
+            # backward
+            train_loss_batch.backward()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            optimizer.step()
+            scheduler.step()
+            model.zero_grad()
 
         ## Validation
         model.eval()
@@ -126,59 +122,61 @@ def model_training(train_dataloader, valid_dataloader, test_dataloader):
             batch = tuple(t.to(DEVICE) for t in batch)
             b_input_ids, b_input_mask, b_labels = batch
             
+            # forward
             with torch.no_grad():
-                outputs = model(b_input_ids, 
-                                token_type_ids=None, 
-                                attention_mask=b_input_mask,
-                                labels=b_labels)
+                outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
             
-            loss = outputs[0]
-            valid_loss += loss.item()
+            # loss
+            valid_loss_batch = outputs.loss
+            valid_loss += valid_loss_batch.item()
 
-            logits = outputs[1]
-            logits = logits.detach().cpu().numpy()
-            label_ids = b_labels.to('cpu').numpy()
-
-            valid_accuracy_batch = clauseeval.flat_accuracy(logits, label_ids)
-            valid_accuracy_epoch_total += valid_accuracy_batch
-            nb_valid_steps += 1
-
-        ## Test
-        model.eval()
-        for batch in test_dataloader:
-            batch = tuple(t.to(DEVICE) for t in batch)
-            b_input_ids, b_input_mask, b_labels = batch
-
-            with torch.no_grad():
-                outputs = model(b_input_ids,
-                                token_type_ids=None,
-                                attention_mask=b_input_mask)
-
-            logits = outputs[0]
-            logits = logits.detach().cpu().numpy()
-            label_ids = b_labels.to('cpu').numpy()
-
-            test_accuracy_batch = clauseeval.flat_accuracy(logits, label_ids)
-            test_accuracy_epoch_total += test_accuracy_batch
-            nb_test_steps += 1
+            # accuracy
+            pred = [torch.argmax(logit).cpu().detach().item() for logit in outputs.logits]
+            true = [label for label in b_labels.cpu().numpy()]
+            valid_accuracy += accuracy_score(true, pred)
 
         ## Report status
         avg_train_loss = train_loss/len(train_dataloader)
+        avg_train_accuracy = train_accuracy/len(train_dataloader)
         avg_valid_loss = valid_loss/len(valid_dataloader)
-        valid_accuracy_epoch = valid_accuracy_epoch_total/nb_valid_steps
-        test_accuracy_epoch = test_accuracy_epoch_total/nb_test_steps
+        avg_valid_accuracy = valid_accuracy/len(valid_dataloader)
         
         result['target_label'].append(target_label)
         result['epoch'].append(epoch+1)
         result['train_loss'].append(avg_train_loss)
         result['valid_loss'].append(avg_valid_loss)
-        result['valid_accuracy'].append(valid_accuracy_epoch)
-        result['test_accuracy'].append(test_accuracy_epoch)
-        log = '  | Epochs: ({}/{})  TrLs: {:.03f}  VlLs: {:.03f}  VlAcc: {:.03f}  TsAcc: {:.03f}'.format(epoch+1, EPOCHS, avg_train_loss, avg_valid_loss, valid_accuracy_epoch, test_accuracy_epoch)
+        result['train_accuracy'].append(avg_train_accuracy)
+        result['valid_accuracy'].append(avg_valid_accuracy)
+        log = '  | Epochs: ({}/{})  TrLs: {:.03f}  TrAcc: {:.03f}  VlLs: {:.03f}  VlAcc: {:.03f}'.format(epoch+1, EPOCHS, avg_train_loss, avg_train_accuracy, avg_valid_loss, avg_valid_accuracy)
         print('\r'+log, end='')
 
     print('\n  | Training complete')
     return model, result
+
+def model_testing(model, test_dataloader):
+    print('------------------------------------------------------------')
+    print('  | Model testing')
+
+    model.eval()
+    test_accuracy = 0
+
+    for batch in test_dataloader:
+        batch = tuple(t.to(DEVICE) for t in batch)
+        b_input_ids, b_input_mask, b_labels = batch
+
+        # forward
+        with torch.no_grad():
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+
+        # accuracy
+        pred = [torch.argmax(logit).cpu().detach().item() for logit in outputs.logits]
+        true = [label for label in b_labels.cpu().numpy()]
+        test_accuracy += accuracy_score(true, pred)
+
+    # Report status
+    avg_test_accuracy = test_accuracy/len(test_dataloader)
+    print('  | Test accuracy: {:.03f}'.format(avg_test_accuracy))
+    return avg_test_accuracy
 
 
 if __name__ == '__main__':
@@ -194,7 +192,7 @@ if __name__ == '__main__':
     test_ratio = round((1-TRAIN_TEST_RATIO)*100)
     
     MAX_SENT_LEN = 128
-    BATCH_SIZE = 32
+    BATCH_SIZE = 4
     RANDOM_STATE = 42
 
     EPOCHS = 1000
@@ -202,8 +200,9 @@ if __name__ == '__main__':
 
     ## Model development
     DEVICE = gpu_allocation()
+    test_result = defaultdict(list)
+
     label_list = ['PAYMENT', 'TEMPORAL', 'METHOD', 'QUALITY', 'SAFETY', 'RnR', 'DEFINITION', 'SCOPE']
-    # label_list = ['DEFINITION', 'SCOPE']
     for target_label in label_list:
         print('============================================================')
         print('Target category: <{}>'.format(target_label))
@@ -219,13 +218,21 @@ if __name__ == '__main__':
         test_inputs, test_masks, test_labels = corpus_res['test']
 
         ## Build dataloader
-        train_dataloader = build_dataloader(inputs=train_inputs_res, labels=train_labels_res, masks=train_masks_res, target_label=target_label, option='train', encode=False)
-        valid_dataloader = build_dataloader(inputs=valid_inputs, labels=valid_labels, masks=valid_masks, target_label=target_label, option='valid', encode=True)
-        test_dataloader = build_dataloader(inputs=test_inputs, labels=test_labels, masks=test_masks, target_label=target_label, option='test', encode=True)
+        train_dataloader = build_dataloader(inputs=train_inputs_res, labels=train_labels_res, masks=train_masks_res, target_label=target_label, encode=False)
+        valid_dataloader = build_dataloader(inputs=valid_inputs, labels=valid_labels, masks=valid_masks, target_label=target_label, encode=True)
+        test_dataloader = build_dataloader(inputs=test_inputs, labels=test_labels, masks=test_masks, target_label=target_label, encode=True)
 
         ## Model training
-        model, result = model_training(train_dataloader=train_dataloader, valid_dataloader=valid_dataloader, test_dataloader=test_dataloader)
+        model, result = model_training(train_dataloader=train_dataloader, valid_dataloader=valid_dataloader)
 
-        ## Export result
-        fname_result = 'result_{}_TR-{}_VL-{}_TS-{}_BS-{}_EP-{}_LR-{}_LB-{}.xlsx'.format(base, train_ratio, valid_ratio, test_ratio, BATCH_SIZE, EPOCHS, LEARNING_RATE, target_label)
-        clauseio.save_result(result=result, fname_result=fname_result)
+        ## Export training result
+        fname_train_result = 'result-bert2_{}_TR-{}_VL-{}_TS-{}_BS-{}_EP-{}_LR-{}_LB-{}_train.xlsx'.format(base, train_ratio, valid_ratio, test_ratio, BATCH_SIZE, EPOCHS, LEARNING_RATE, target_label)
+        clauseio.save_result(result=result, fname_result=fname_train_result)
+
+        ## Model testing
+        test_accuracy = model_testing(model=model, test_dataloader=test_dataloader)
+        test_result[target_label].append(test_accuracy)
+
+    ## Export testing result
+    fname_test_result = 'result-bert2_{}_TR-{}_VL-{}_TS-{}_BS-{}_EP-{}_LR-{}_test.xlsx'.format(base, train_ratio, valid_ratio, test_ratio, BATCH_SIZE, EPOCHS, LEARNING_RATE)
+    clauseio.save_result(result=result, fname_result=fname_test_result)
